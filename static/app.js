3553c6132027176c192f14a687cc3f65e35510e8
= <=>
2023-10-11 19:24:16 -0300

    Aprimoramento do envio de mensagem

--- static/app.js
+++ static/app.js
@@ -1,30 +1,62 @@
-class FormSubmit{
-    constructor(settings){
+class FormSubmit {
+    constructor(settings) {
         this.settings = settings;
         this.form = document.querySelector(settings.form);
-        if (this.form){
+        if (this.form) {
             this.url = this.form.getAttribute("action");
         }
     }
 
-    displaySuccess(){
-        this.form.innerHTML = this.settings.success;
+    displaySuccess() {
+        const successMessage = document.getElementById("success-message");
+        successMessage.style.display = "block";
+
+        // Rolando para o início após 1 segundos
+        setTimeout(() => {
+            window.scrollTo(0, 0);
+            // Recarregando a página após 1 segundos
+            setTimeout(() => {
+                location.reload();
+            }, 1000); // Ajuste o tempo conforme necessário
+        }, 1000); // Ajuste o tempo conforme necessário
     }
 
-    displayError(){
-        this.form.innerHTML = this.settings.error;
+    displayError() {
+        const errorMessage = document.getElementById("error-message");
+        errorMessage.style.display = "block";
+        setTimeout(() => {
+            errorMessage.style.display = "none";
+        }, 1000); // Oculta a mensagem de erro após 1 segundos
     }
 
-    init(){
-        if (this.form) this.formButton.addEventListener("click", () => this.displaySuccess());
-        return this;
+    init() {
+        if (this.form) {
+            this.form.addEventListener("submit", async (event) => {
+                event.preventDefault();
+                const formData = new FormData(this.form);
+
+                try {
+                    const response = await fetch(this.url, {
+                        method: "POST",
+                        body: formData,
+                    });
+
+                    if (response.ok) {
+                        this.displaySuccess();
+                    } else {
+                        this.displayError();
+                    }
+                } catch (error) {
+                    this.displayError();
+                }
+            });
+        }
     }
 }
 
 const formSubmit = new FormSubmit({
     form: "[data-form]",
     button: "[data-button]",
-    success: "<h1 class='success'>Mensagem enviada!</h1>",
-    error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
 });
 formSubmit.init();
+
